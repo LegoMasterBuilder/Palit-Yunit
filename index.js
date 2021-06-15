f3006307934b7687825d28cc83abee1888001138
banseto11@gmail.com <hqlorico@up.edu.ph>
2021-06-15 12:25:48 +0800

    latest stable version

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-// 
-function copytoclipboard() {
-  /* Get the text field */
+
+function myFunction() {
   var copyText = document.getElementById("myInput");
-
-  /* Select the text field */
   copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
+  copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
-
-  // /* Alert the copied text */
-  // alert("Copied the text: " + copyText.value);
 }
 
 // Function to switch drop down menu
