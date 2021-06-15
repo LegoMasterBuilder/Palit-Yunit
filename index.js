8e692bfc41b17267174fd5af070f53955d917999
banseto11@gmail.com <hqlorico@up.edu.ph>
2021-06-15 14:06:41 +0800

    Added Open Graph

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function myFunction() {
   document.execCommand("copy");
 }
 
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+  fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 // Function to switch drop down menu
 
 function exchangeDropDown(){
